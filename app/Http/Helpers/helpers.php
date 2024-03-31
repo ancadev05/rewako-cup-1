ebbd686e4a112a6fc061a69dd41a3ab2d869c7e0
hdev05 <hamzah.dev.061223@gmail.com>
2024-04-01 07:32:39 +0800

    perbaiki isi helper

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -1,29 +1,52 @@
 <?php
 
-// format uang
-
-
+// format uang indonesia
 function format_uang($angka)
 {
   return number_format($angka, 0, ',', '.');
 }
 
-// angka terbilang
-function terbilang($angka)
-{
-  $angka = abs($angka);
-  $baca = array('', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas');
-  $terbilang = '';
-
-  if ($angka < 12) {
-    $terbilang = '' . $baca[$angka];
-  } elseif( $angka < 20) {
-    $terbilang = terbilang($angka - 20) . ' belas';
+// format rupiah angka terbilang
+function terbilang($nilai) {
+  if($nilai<0) {
+      $hasil = "minus ". trim(ke_kata($nilai));
+  } else {
+      $hasil = trim(ke_kata($nilai));
   }
-
+  return $hasil;
 }
 
+function ke_kata($nilai) {
+  $nilai = abs($nilai);
+  $huruf = array("", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
+  $temp = "";
+  if ($nilai < 12) {
+      $temp = " ". $huruf[$nilai];
+  } else if ($nilai <20) {
+      $temp = ke_kata($nilai - 10). " belas";
+  } else if ($nilai < 100) {
+      $temp = ke_kata($nilai/10)." puluh". ke_kata($nilai % 10);
+  } else if ($nilai < 200) {
+      $temp = " seratus" . ke_kata($nilai - 100);
+  } else if ($nilai < 1000) {
+      $temp = ke_kata($nilai/100) . " ratus" . ke_kata($nilai % 100);
+  } else if ($nilai < 2000) {
+      $temp = " seribu" . ke_kata($nilai - 1000);
+  } else if ($nilai < 1000000) {
+      $temp = ke_kata($nilai/1000) . " ribu" . ke_kata($nilai % 1000);
+  } else if ($nilai < 1000000000) {
+      $temp = ke_kata($nilai/1000000) . " juta" . ke_kata($nilai % 1000000);
+  } else if ($nilai < 1000000000000) {
+      $temp = ke_kata($nilai/1000000000) . " milyar" . ke_kata(fmod($nilai,1000000000));
+  } else if ($nilai < 1000000000000000) {
+      $temp = ke_kata($nilai/1000000000000) . " triliun" . ke_kata(fmod($nilai,1000000000000));
+  }     
+  return $temp;
+}
+
+// membuat format tanggal indonesia
 function tanggalIndonesia($tanggal){
+  // format input tanggal date('Y-m-d')
   // Array hari dan bulan
   $hari = array ( 1 => 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu');
   $bulan = array (1 => 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember');
@@ -38,3 +61,18 @@ function tanggalIndonesia($tanggal){
   // Menggabungkan hasil format tanggal
   return $hari[$numHari] . ', ' . $split[2] . ' ' . $bulan[$numBulan] . ' ' . $split[0];
 }
+
+// mengambil huruf pertama di setiap kata
+function ambilHurufAwal($string) {
+  // Memecah string menjadi array kata
+  $kata = explode(" ", $string);
+  $hasil = "";
+
+  // Iterasi melalui setiap kata dan ambil huruf pertama
+  foreach ($kata as $k) {
+      $hasil .= $k[0];
+  }
+
+  return $hasil;
+}
+
